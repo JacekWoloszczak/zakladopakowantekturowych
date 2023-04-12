ff16ac01193d98c7b165be02d930d366ce33b492
JacekWoloszczak <jacek.woloszczak@gmail.com>
2023-04-12 15:40:39 +0200

    Script
    New element

--- js/script.js
+++ js/script.js
@@ -9,3 +9,21 @@
     menuOverlay.classList.add("d-none");
   });
 })();
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 300;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
