2a759a5fae89837d9f21ac230a431cb201d3f280
JacekWoloszczak <jacek.woloszczak@gmail.com>
2023-06-22 23:19:30 +0200

    index

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,6 @@
 const button = document.querySelector("#cookie-btn");
 const divCookie = document.querySelector("#cookie");
+const divHide = document.querySelector("#is-hide");
 button.addEventListener("click", () => {
   divCookie.remove();
 });
