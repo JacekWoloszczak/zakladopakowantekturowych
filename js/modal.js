f36751d61f99996808fb1c35b34d498c31ed1274
JacekWoloszczak <jacek.woloszczak@gmail.com>
2023-07-09 13:53:07 +0200

    cookiw.html;

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,16 @@
-const button = document.querySelector("#cookie-btn");
-const divCookie = document.querySelector("#cookie");
-const divHide = document.querySelector("#is-hide");
-button.addEventListener("click", () => {
-  divCookie.remove();
-});
+const button = document.querySelector(".cookie-btn");
+const divCookie = document.querySelector(".cookie");
+
+const searchCookie = () => {
+  const eatenCookie = localStorage.getItem("cookie");
+  if (eatenCookie) {
+    divCookie.classList.add("hide");
+  }
+};
+const handledivCookie = () => {
+  localStorage.setItem("cookie", "true");
+  divCookie.classList.add("hide");
+};
+
+button.addEventListener("click", handledivCookie);
+searchCookie();
