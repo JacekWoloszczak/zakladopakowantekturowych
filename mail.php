9f6fdfefcf4735f5e7d28a644ef67e3c0950c588
JacekWoloszczak <jacek.woloszczak@gmail.com>
2023-07-27 23:35:53 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,10 +1,10 @@
 <?php
 
-$to ='opakowania.lomianki@gmail.com';
-$name = $_POST['name'];
-$email = $_POST['email'];
+$to ='j_w07@wp.pl';
+// $name = $_POST['name'];
+// $email = $_POST['email'];
 $subject = 'Nowy mail od'. $name . '('. $email . ')';
-$message = $_POST['message'];
+// $message = $_POST['message'];
 $headers = 'From:' . $name . '('. $email . ')';
 $headers .= 'Content-Type: text/plain; charset=utf-8' . "\r\n";
 
